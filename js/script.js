2878bfabfc979c6ca2eee9883819278927b699a6
Dan848 <daniele.ceravolo15@gmail.com>
2023-03-27 09:16:02 +0200

    add random color for type button

--- js/script.js
+++ js/script.js
@@ -15,29 +15,6 @@ BONUS
 2- popolare le options della select della milestone 3 dinamicamente.
 */
 
-//Funzione che genera codice esadecimale casuale
-function getRndColor (){
-    function getRndInteger(min, max) {
-    return Math.floor(Math.random() * (max - min) + 1) + min;
-    }
-
-    let rndColor = "#"
-    for (let i = 0; i < 6; i++){
-        let rndDigit = getRndInteger(0, 16);
-        if(rndDigit === 10) rndColor += "A";
-        else if(rndDigit === 11) rndColor += "A";
-        else if(rndDigit === 12) rndColor += "B";
-        else if(rndDigit === 13) rndColor += "C";
-        else if(rndDigit === 14) rndColor += "D";
-        else if(rndDigit === 15) rndColor += "E";
-        else if(rndDigit === 16) rndColor += "F";
-        else rndColor += rndDigit;
-    }
-    console.log(rndColor);
-}
-
-getRndColor()
-
 //Array di oggetti
 const icons = [
     {
@@ -159,6 +136,42 @@ const arrFullType = icons.map((icon) => icon.type).filter(uniqueType);
 //Pusho all nell'array (poiché non è un type) usando unshift, così che al primo avvio le card verranno mostrate tutte
 arrFullType.unshift("all");
 
+//Funzione che genera codice esadecimale casuale
+function getRndColor (){
+    function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min) + 1) + min;
+    }
+
+    let rndColor = "#"
+    for (let i = 0; i < 6; i++){
+        let rndDigit = getRndInteger(0, 16);
+        if(rndDigit === 10) rndColor += "A";
+        else if(rndDigit === 11) rndColor += "A";
+        else if(rndDigit === 12) rndColor += "B";
+        else if(rndDigit === 13) rndColor += "C";
+        else if(rndDigit === 14) rndColor += "D";
+        else if(rndDigit === 15) rndColor += "E";
+        else if(rndDigit === 16) rndColor += "F";
+        else rndColor += rndDigit;
+    }
+    return rndColor;
+}
+
+getRndColor()
+
+function changeIconsColor () {
+    for (let i = 0; i < arrFullType.length; i++) {
+        let newColor = getRndColor();
+        icons.forEach((icon, index) =>{
+            if (icon.type === arrFullType[i]) icon.color = newColor;
+        })
+    }
+    renderCard();
+}
+
+
+
+//Funzione per filtrare i doppioni in un array
 function uniqueType (value, index, arr) {
     return arr.indexOf(value) === index;
 }
@@ -176,10 +189,10 @@ function renderSelect() {
 
 
     const header = document.querySelector(".bg-gradient");
-    header.innerHTML = `<label for="iconType">Filtra per tipo</label>
+    header.innerHTML += `<div class=""><label for="iconType">Filtra per tipo</label>
     <select name="iconType" id="iconType" class=" text-white ms-2 bg-dark border-0 rounded-2 px-2">
         ${createOptions()}
-    </select>`;
+    </select></div>`;
     return header;
 }
 
@@ -187,7 +200,7 @@ function renderSelect() {
 function createCard (ico) {
     const card = `<div class="col mb-4 fs-1 text-center">
     <div class="card bg-black p-3">
-        <i class="${ico.prefix}${ico.family} ${ico.prefix}${ico.name} ${ico.color}" style="color: "></i>
+        <i class="${ico.prefix}${ico.family} ${ico.prefix}${ico.name} ${ico.type}" style="color: "></i>
         <div class="text-uppercase text-white fw-medium mt-2 fs-6">${ico.name}</div>
     </div>
 </div>`;
@@ -197,9 +210,11 @@ return card;
 //Fuzione per renderizzare le card in base al valore della select
 function renderCard () {
     //Assegno i colori alle variabli css
-    document.documentElement.style.setProperty("--animal-color", "orange");
-    document.documentElement.style.setProperty("--vegetable-color", "green");
-    document.documentElement.style.setProperty("--user-color", "blue");
+    
+    icons.forEach((icon) =>{
+        document.documentElement.style.setProperty(`--${icon.type}-color`, `${icon.color}`)
+    });
+
     const row = document.querySelector(".row");
     let cards = ``;
 
@@ -230,3 +245,6 @@ renderSelect();
 const select = document.querySelector("select");
 select.addEventListener("change", renderCard);
 renderCard(select);
+
+const colorButton = document.querySelector("button");
+colorButton.addEventListener("click", changeIconsColor);
